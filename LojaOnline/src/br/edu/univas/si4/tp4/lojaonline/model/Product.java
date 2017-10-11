a08888869282e0c3052bcdd02523eb68a79c9e06
unknown <aluno0402@labf426184.fuvs.br>
2017-10-11 20:36:09 -0300

    Exercise about Java Swing - Commit 3

--- LojaOnline/src/br/edu/univas/si4/tp4/lojaonline/model/Product.java
+++ LojaOnline/src/br/edu/univas/si4/tp4/lojaonline/model/Product.java
@@ -2,5 +2,34 @@ package br.edu.univas.si4.tp4.lojaonline.model;
 
 public class Product {
 
+	private String name;
+	private int quantity;
+	
+	
+	
+	public Product(String name, int quantity) {
+		super();
+		this.name = name;
+		this.quantity = quantity;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", quantity=" + quantity + "]";
+	}
+	
 	
 }
--- LojaOnline/src/br/edu/univas/si4/tp4/lojaonline/model/ProductDAO.java
+++ LojaOnline/src/br/edu/univas/si4/tp4/lojaonline/model/ProductDAO.java
@@ -3,9 +3,15 @@ package br.edu.univas.si4.tp4.lojaonline.model;
 import java.util.ArrayList;
 
 public class ProductDAO {
-	public ArrayList<Product> listByname(String name){
-		// TODO: 
-		return null;
+	public ArrayList<Product> listByname(String name){		
+		ArrayList<Product> result = new ArrayList<Product>();
+		
+		result.add(new Product("Arroz", 10));
+		result.add(new Product("Feijão", 50));
+		result.add(new Product("Farinha", 15));
+		result.add(new Product("Macarrão", 5));
+		
+		return result;
 		
 	}
 	
